8f94c66565437dbeb6e8ec05d9121ca49df3a92d
So-diu <suddd@kakao.com>
2026-05-05 01:34:29 +0900

    Initial commit - publish digital textbook

---
+++ web/astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://somath-edu.github.io',
+	base: '/h1_math_c1',
+	output: 'static',
+});
